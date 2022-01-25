7d50ac0ff8ecb352c45d6a9e8ed656c4507a600f
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-01-25 20:45:20 +0700

    Runtime Permission DAN Get Loc dengan Location Manager

--- app/src/main/java/com/nandaiqbalh/earthweather/WeatherController.java
+++ app/src/main/java/com/nandaiqbalh/earthweather/WeatherController.java
@@ -1,11 +1,40 @@
 package com.nandaiqbalh.earthweather;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class WeatherController extends AppCompatActivity {
+
+    // Declare Constant
+    final int REQUEST_CODE = 123;
+    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    // App ID to use OpenWeather data
+    final String APP_ID = "0eab0d218a11b964b2c23bf96f0fb714";
+    // Time between location updates (5000 milliseconds or 5 seconds)
+    final long MIN_TIME = 5000;
+    // Distance between location updates (1000m or 1km)
+    final float MIN_DISTANCE = 1000;
+    // LogCat tag
+    final String LOGCAT_TAG = "EW";
+
+    // Set Location Provider
+    String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
+
+    // Location Manager(Request or Stop) and Location Listener(Memonitor jika ada perubahan)
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +44,80 @@ public class WeatherController extends AppCompatActivity {
         // full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        getWeatherForCurrentLocation();
+
+    }
+
+    private void getWeatherForCurrentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                Log.d(LOGCAT_TAG, "OnLocationChanged() called");
+
+                // menambahkan String, untuk mendapatkan longitude dan latitude
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
+
+                // tampilkan longitude dan latitude ke LogCat
+                Log.d(LOGCAT_TAG, "The longitude is : " + longitude);
+                Log.d(LOGCAT_TAG, "The latitude is : " + latitude);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+                Log.d(LOGCAT_TAG, "onProviderDisabled() called");
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String [] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE){
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(LOGCAT_TAG, "Permission Granted!");
+                // jika diizinkan dan true maka akan panggil method di bawah, dan masuk method onLocationChanged.
+                getWeatherForCurrentLocation();
+            }
+            // jika tidak dapat izin maka permission denied, dan akan muncul Toast message
+        } else {
+            Log.d(LOGCAT_TAG, "Permission Denied!");
+            Toast.makeText(getApplicationContext(), "Failed! location access denied", Toast.LENGTH_SHORT).show();
+        }
     }
 }
